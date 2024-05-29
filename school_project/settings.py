5d0704fe7b8f8342ebf62fbfb00c30bfa8b085b8
Danis <dandijar@yandex.ru>
2024-05-29 20:54:02 +0500

    vercel ready?

--- school_project/settings.py
+++ school_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!x)1y#&q-#d#!droz79w!w6^to-m68z+de+qawf72n(+2q(6yv'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
