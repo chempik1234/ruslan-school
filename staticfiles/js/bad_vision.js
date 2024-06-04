802f9b0578f833bab4858f164885f17a15ec4d01
Danis <dandijar@yandex.ru>
2024-06-04 20:34:57 +0500

    bad vision + fixes

---
+++ staticfiles/js/bad_vision.js
@@ -0,0 +1,14 @@
+const toggleButton = document.getElementById('toggle-vision');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  if (body.classList.contains('bad-vision')) {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    body.classList.remove('bad-vision');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'bad-vision');
+    localStorage.setItem('theme', 'bad-vision');
+    body.classList.add('bad-vision');
+  }
+});
