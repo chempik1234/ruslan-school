cb60295d31ea25d4ef6e9785a3a4325822baa799
Danis <dandijar@yandex.ru>
2024-06-10 15:30:06 +0500

    bad vision color-changing

--- static/js/bad_vision.js
+++ static/js/bad_vision.js
@@ -1,7 +1,30 @@
-const toggleButton = document.getElementById('toggle-vision');
 const body = document.body;
 
+var currentTextSizeClass = "";
+function changeTextSize(sizeClass) {
+    if (currentTextSizeClass != ""){
+        body.classList.remove(currentTextSizeClass);
+    }
+    body.classList.add(sizeClass);
+    currentTextSizeClass = sizeClass;
+}
+var currentColorSchemeClass = "";
+function changeColorScheme(colorSchemeClass) {
+    if (currentColorSchemeClass != ""){
+        body.classList.remove(currentColorSchemeClass);
+    }
+    body.classList.add(colorSchemeClass);
+    currentColorSchemeClass = colorSchemeClass;
+}
+
+const toggleButton = document.getElementById('toggle-vision');
 toggleButton.addEventListener('click', () => {
+  if (currentTextSizeClass == "") {
+      changeTextSize('text-size-100');
+  }
+  if (currentColorSchemeClass == "") {
+    changeColorScheme('color-scheme-white');
+  }
   if (body.classList.contains('bad-vision')) {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
@@ -12,13 +35,3 @@ toggleButton.addEventListener('click', () => {
     body.classList.add('bad-vision');
   }
 });
-
-
-var currentTextSizeClass = "";
-function changeTextSize(sizeClass) {
-    if (currentTextSizeClass != ""){
-        body.classList.remove(currentTextSizeClass);
-    }
-    body.classList.add(sizeClass);
-    currentTextSizeClass = sizeClass;
-}
