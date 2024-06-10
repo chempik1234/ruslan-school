b41663713390e1d9dcc7cfd354a1e7b3ebb2ca0a
Danis <dandijar@yandex.ru>
2024-06-10 14:14:25 +0500

    общеобразовательная

--- main_pages/context_processors.py
+++ main_pages/context_processors.py
@@ -6,7 +6,7 @@ def default_context(request):
         'company_address': "123456, респ. Башкортостан, г. Туймазы, ул. Уличная, д. 1",
         'company_phone': "+7 (999) 777-77-77",
         "company_email": "school0@email.ru",
-        "company_name": "МБОУ средняя общая образовательная школа №0 г.Туймазы",
+        "company_name": "МБОУ средняя общеобразовательная школа №0 г.Туймазы",
         "news": News.objects.all()
         # "side_pictures":
     }
